7f8e6aa78ae8a4774f6adaebd37328706425a1cc
u211-04 <u211-04@public.mpei.local>
2018-05-15 09:47:42 +0300

    assert

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<cassert>
+using namespace std;
+char nibble_to_hex(uint8_t i)
+{
+char digits[]="0123456789abcdef";
+return digits[i];
+}
+
+
+
+
+int main()
+{
+    assert(nibble_to_hex(0x0) == '0');
+    assert(nibble_to_hex(0x1) == '1');
+    assert(nibble_to_hex(0x2) == '2');
+    assert(nibble_to_hex(0x3) == '3');
+    assert(nibble_to_hex(0x4) == '4');
+    assert(nibble_to_hex(0x5) == '5');
+    assert(nibble_to_hex(0x6) == '6');
+    assert(nibble_to_hex(0x7) == '7');
+    assert(nibble_to_hex(0x8) == '8');
+    assert(nibble_to_hex(0x9) == '9');
+    assert(nibble_to_hex(0xa) == 'a');
+    assert(nibble_to_hex(0xb) == 'b');
+    assert(nibble_to_hex(0xc) == 'c');
+    assert(nibble_to_hex(0xd) == 'd');
+    assert(nibble_to_hex(0xe) == 'e');
+    assert(nibble_to_hex(0xf) == 'f');
+
+}
