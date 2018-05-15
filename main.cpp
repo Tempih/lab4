1d9d6e17421183ac68a3f7f46ba622c016832665
u211-04 <u211-04@public.mpei.local>
2018-05-15 10:41:29 +0300

    print_in_binary

--- main.cpp
+++ main.cpp
@@ -31,8 +31,32 @@ void print_in_hex(const void* data, size_t size) {
         }
     }
 }
+char bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
+void print_in_binary(uint8_t byte) {
+    for (uint8_t bit = 7; bit >0; bit--) {
+        cout << bit_digit(byte, bit);
+    }
+    cout<<bit_digit(byte,0);
+}
 
+    void print_in_binary(const void* data, size_t size) {
+        const uint8_t* bytes = as_bytes(data);
+        for (size_t i = 0; i < size; i++) {
+            print_in_binary(bytes[i]);
 
+            if ((i + 1) % 4 == 0) {
+                cout << '\n';
+            }
+            else {
+                cout << ' ';
+            }
+        }
+    }
 
 
 int main()
@@ -57,5 +81,8 @@ int main()
     cout << "u32 bytes: ";
     print_in_hex(&u32, sizeof(u32));
     cout << '\n';
-
+    uint32_t u8 = 3;
+    cout << "u8 bytes: ";
+    print_in_binary(&u8, sizeof(u8));
+    cout << '\n';
 }
